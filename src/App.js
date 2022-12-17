9c8137c11c0c91f39f72015630fe7bcf55675b44
jagadhis <jwaran78@gmail.com>
2022-12-17 14:03:20 +0530

    Added Skills

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import NavBar from './Components/NavBar';
 import Banner from './Components/Banner';
+import Skills from './Components/Skills';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
     <div className="App">
     <NavBar/>
     <Banner/>
+    <Skills/>
     </div>
   );
 }
