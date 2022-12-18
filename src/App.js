686089e544c1099e23442835fdd4ac09dc6fff91
jagadhis <jwaran78@gmail.com>
2022-12-18 14:25:58 +0530

    Added Footer And Env

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Banner from './Components/Banner';
 import Skills from './Components/Skills';
 import Projects from './Components/Projects';
 import Contact from './Components/Contact';
+import Footer from './Components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
     <Skills/>
     <Projects/>
     <Contact />
+    <Footer/>
     </div>
   );
 }
