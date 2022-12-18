64455680b12e12d26ca46346be42df1a9dbbc7ea
jagadhis <jwaran78@gmail.com>
2022-12-18 14:55:36 +0530

    Changes

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,63 +1,78 @@
-import React , {useEffect, useState} from 'react'
-import { Container,Row,Col } from 'react-bootstrap'
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assets/img/header-img.svg";
+import "animate.css";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import TrackVisibility from "react-on-screen";
+
 
 const Banner = () => {
-    const [loopNum,setloopNum] = useState(0);
-    const [isDelete,setIsDelete] = useState(false);
-    const toRotate = ["Web Developer","Full Stack Developer","UI/UX Designer"];
-    const [text,setText]= useState('')
-    const [delta,setDelta]= useState(300- Math.random() * 100);
-    const period = 2000;
+  const [loopNum, setloopNum] = useState(0);
+  const [isDelete, setIsDelete] = useState(false);
+  const toRotate = ["Web Developer", "Full Stack Developer", "UI/UX Designer"];
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const period = 2000;
 
-    useEffect(()=>{
-        let ticker = setInterval(()=>[
-            tick()
-        ],delta)
+  useEffect(() => {
+    let ticker = setInterval(() => [tick()], delta);
 
-        return () => {clearInterval(ticker)};
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
 
-    },[text])
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDelete
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+    setText(updatedText);
 
-    const tick = () => {
-        let i = loopNum % toRotate.length;
-        let fullText = toRotate[i];
-        let updatedText = isDelete ? fullText.substring(0,text.length -1 ) : fullText.substring(0,text.length + 1)
-        setText(updatedText);
-
-        if(isDelete){
-            setDelta(prevDelta => prevDelta/2)
-        }
-
-        if(!isDelete && updatedText === fullText){
-                setIsDelete(true);
-                setDelta(period)
-
-        }else if(isDelete && updatedText === ""){
-            setIsDelete(false);
-            setloopNum(loopNum+1);
-            setDelta(500)
+    if (isDelete) {
+      setDelta((prevDelta) => prevDelta / 2);
     }
-}
-    
+
+    if (!isDelete && updatedText === fullText) {
+      setIsDelete(true);
+      setDelta(period);
+    } else if (isDelete && updatedText === "") {
+      setIsDelete(false);
+      setloopNum(loopNum + 1);
+      setDelta(500);
+    }
+  };
+
   return (
-   <section className='banner' id='home'>
-    <Container>
-        <Row className='align-items-center'>
-<Col xs={12} md={6} xl={7}>
-    <span className='tagline'>Welcome To My portfolio</span>
-    <h1>{`Hi I am jagadhiswaran `}<span className='wrap'>{text}</span></h1>
-    <p>About Me</p>
-    <button onClick={()=>console.log('connect')}>Let's Connect</button>
-</Col>
-<Col xs={12} md={6} xl={5}>
-  <img src={headerImg} alt="Header Img"/>
-</Col>
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+            {({isVisible})=> 
+              <div
+                className={isVisible ? "animated_animated animate_fadeIn" : ""}
+              >
+                <span className="tagline">Welcome To My portfolio</span>
+                <h1>
+                  {`Hi I am jagadhiswaran `}
+                  <span className="wrap">{text}</span>
+                </h1>
+                <p>About Me</p>
+                <button onClick={() => console.log("connect")}>
+                  Let's Connect <ArrowRightCircle size={25} />
+                </button>
+              </div>}
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={headerImg} alt="Header Img" />
+          </Col>
         </Row>
-    </Container>
+      </Container>
+    </section>
+  );
+};
 
-   </section>
-  )
-}
-
-export default Banner
+export default Banner;
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -5,6 +5,8 @@ import proImg3 from "../assets/img/project-img3.png"
 import ProjectCard from './ProjectCard';
 import colorSharp2 from "../assets/img/color-sharp2.png"
 import { Container, Row,Col,Nav, Tab } from 'react-bootstrap'
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
 
 const Projects = () => {
     const projects =[
@@ -29,37 +31,49 @@ const Projects = () => {
    <section className='project' id='project'>
 <Container>
 <Row>
-    <Col>
-    <h2>projects</h2>
-    <p>About projects</p>
-    <Tab.Container id='project-tabs' defaultActiveKey='first'>
-    <Nav variant="pills" className='nav-pills mb-5 justify-content-center align-items-center' id='pills-tab'>
-      <Nav.Item>
-        <Nav.Link eventKey="first" href="/home">Tab One</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="second">Tab Two</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="third" >
-          Tab three
-        </Nav.Link>
-      </Nav.Item>
-    </Nav>
-    <Tab.Content>
-        <Tab.Pane eventKey='first'>
-            <Row>
-                {projects.map((project,index) => {
-                    return(
-                     <ProjectCard key={index} {...project}/>
-                    )
-                })}
-            </Row>
-        </Tab.Pane>
-        <Tab.Pane eventKey='second'></Tab.Pane>
-        <Tab.Pane eventKey='third'></Tab.Pane>
-    </Tab.Content>
-    </Tab.Container>
+    <Col size={12}>
+    <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projects</h2>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="section">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
 
     </Col>
 </Row>
